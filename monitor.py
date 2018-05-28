2b3dcd02d297f565af214c303ce118b692979456
Ravi Jethani <rjethani@cisco.com>
2018-05-28 06:44:14 +0000

    Added signal handling
    Code can now handle SIGINT and SIGTERM signals.
    Refactored code to make code configurable via
    environment variables as much as possible

--- monitor.py
+++ monitor.py
@@ -8,19 +8,22 @@ import jenkins
 import shutil
 import time
 import logging
+import signal
 
 
+##### GET required values from Environment variables #####
 try:
     JENKINS_USER = os.environ["JENKINS_USER"]
     JENKINS_PASS = os.environ["JENKINS_PASS"]
     JENKINS_NODE = os.environ["JENKINS_NODE"]
+    DISK_THRESHOLD = int(os.getenv("DISK_THRESHOLD", "70"))
+    CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", "5"))
 except KeyError as e:
     raise SystemExit("environment variable {} not found".format(e))
-
+except ValueError as e:
+    raise SystemExit("Error: Not an integer: ".format(e.args[0].split(":")[-1]))
 
 JENKINS_URL = os.getenv("JENKINS_URL", "https://engci-private-sjc.cisco.com/jenkins/iotsp/")
-DISK_THRESHOLD = int(os.getenv("DISK_THRESHOLD", "70"))
-CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", "5"))
 DOCKER_HOST_URL = os.getenv("DOCKER_HOST_URL", "unix://docker.sock")
 DOCKER_ROOT_DIR = os.getenv("DOCKER_ROOT_DIR", "/docker")
 DOCKER_KEEP_IMAGES_UNTIL = os.getenv("DOCKER_KEEP_IMAGES_UNTIL", "72"
@@ -28,38 +31,48 @@ DOCKER_API_VERSION = os.getenv("DOCKER_API_VERSION", "1.30")
 WORKSPACE_ROOT_DIR = os.getenv("WORKSPACE_ROOT_DIR", "/workspace")
 
 
+##### Configure logging #####
 logging.basicConfig(level=logging.INFO, format="%(asctime)s : %(levelname)s : %(funcName)s : %(message)s")
 
+##### Configure handling of Ctrl+C(SIGINT) , docker stop(SIGTERM) signals #####
+def signal_handler(sigid, frame):
+    log.warning("SIGINT/SIGTERM signal received")
+    if STAGE >= 1:
+        log.warning("signal received during cleanup, aborting cleanup and putting node back online, the cleanup may not have completed properly")
+        make_node_online()
+    raise SystemExit(sigid)
 
-def make_node_online(jenkins_info):
-    node = jenkins_info["node"]
-    logging.info("trying to put node '{}' back online".format(node))
-    server = jenkins.Jenkins(jenkins_info["url"], username=jenkins_info["user"], password=jenkins_info["pass"])
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+
+def make_node_online():
+    logging.info("trying to put node '{}' back online".format(JENKINS_NODE))
+    server = jenkins.Jenkins(JENKINS_URL, username=JENKINS_USER, password=JENKINS_PASS)
     server.get_version()
-    server.enable_node(node)
-    logging.info("node '{}'is now online".format(node))
+    server.enable_node(JENKINS_NODE)
+    logging.info("node '{}' is now online".format(JENKINS_NODE))
     return True
 
 
-def make_node_offline(jenkins_info):
-    node = jenkins_info["node"]
-    logging.info("checking node '{}' status".format(node))
-    server = jenkins.Jenkins(jenkins_info["url"], username=jenkins_info["user"], password=jenkins_info["pass"])
+def make_node_offline():
+    logging.info("checking node '{}' status".format(JENKINS_NODE))
+    server = jenkins.Jenkins(JENKINS_URL, username=JENKINS_USER, password=JENKINS_PASS)
     server.get_version()
-    node_info = server.get_node_info(node)
-    if node_info["idle"]:
-        logging.info("node '{}' is currently idle, trying to put it offline".format(node))
-        server.disable_node(node, msg="jenkins-worker-monitor: putting offline for cleanup")
-        logging.info("node '{}' is now offline".format(node))
+    node_idle = server.get_node_info(JENKINS_NODE)["idle"]
+    if node_idle:
+        logging.info("node '{}' is currently idle, trying to put it offline".format(JENKINS_NODE))
+        server.disable_node(JENKINS_NODE, msg="jenkins-worker-monitor: putting offline for cleanup")
+        logging.info("node '{}' is now offline".format(JENKINS_NODE))
         return True
     else:
-        logging.warning("node '{}' is currently not idle, cannot put it offline".format(node))
+        logging.warning("node '{}' is currently not idle, cannot put it offline".format(JENKINS_NODE))
         return False
 
 
-def cleanup_docker(docker_host_url, keep_images_until):
+def cleanup_docker():
     logging.info("starting docker cleanup")
-    docker_client = docker.DockerClient(base_url=docker_host_url, version="1.25")
+    docker_client = docker.DockerClient(base_url=DOCKER_HOST_URL, version=DOCKER_API_VERSION)
 
     byts = docker_client.containers.prune()["SpaceReclaimed"]
     logging.info("cleaned up stopped/stale containers, space reclaimed: {} bytes".format(byts))
@@ -67,78 +80,57 @@ def cleanup_docker(docker_host_url, keep_images_until):
     byts = docker_client.volumes.prune()["SpaceReclaimed"]
     logging.info("cleaned up unused/dangling volumes, space reclaimed: {} bytes".format(byts))
 
-    byts = docker_client.images.prune(filters={"dangling": False, "until": keep_images_until + "h"})["SpaceReclaimed"]
+    byts = docker_client.images.prune(filters={"dangling": False, "until": DOCKER_KEEP_IMAGES_UNTIL + "h"})["SpaceReclaimed"]
     logging.info("cleaned up all dangling images and unused images(older than {} hours), space reclaimed: {} bytes".format(until, byts))
 
     logging.info("finished docker cleanup")
 
 
-def cleanup_workspace(workspace_root_dir):
-    logging.info("starting workspace cleanup at '{}'".format(workspace_root_dir))
-    shutil.rmtree(workspace_root_dir, ignore_errors=True)
-    os.makedirs(workspace_root_dir, exist_ok=True)
+def cleanup_workspace():
+    logging.info("starting workspace cleanup at '{}'".format(WORKSPACE_ROOT_DIR))
+    shutil.rmtree(WORKSPACE_ROOT_DIR, ignore_errors=True)
+    os.makedirs(WORKSPACE_ROOT_DIR, exist_ok=True)
     logging.info("finished workspace cleanup")
 
 
-def check_disk_usage(path, threshold):
+def check_disk_usage(path):
     total, used, available = shutil.disk_usage(path)
     used_perc = math.ceil(used / total * 100)
 
-    critical = used_perc >= threshold
+    critical = used_perc >= DISK_THRESHOLD
     if critical:
-        logging.critical("disk usage of {}: ~{}% [threshold: {}%]".format(path, used_perc, threshold))
+        logging.critical("disk usage of {}: ~{}% [DISK_THRESHOLD: {}%]".format(path, used_perc, DISK_THRESHOLD))
     else:
-        logging.info("disk usage of {}: ~{}% [threshold: {}%]".format(path, used_perc, threshold))
+        logging.info("disk usage of {}: ~{}% [DISK_THRESHOLD: {}%]".format(path, used_perc, DISK_THRESHOLD))
 
     return critical
     
+STAGE = 0
 
 def main():
-    docker_keep_images_until = os.getenv("KEEP_IMAGES_UNTIL", "72")
-    threshold = int(os.getenv("DISK_THRESHOLD", "70"))
-    minutes = int(os.getenv("CHECK_INTERVAL", "5"))
-    jenkins_url = os.getenv("JENKINS_URL", "https://engci-private-sjc.cisco.com/jenkins/iotsp/")
-
-    try:
-        node_name = os.environ["NODE_NAME"]
-        jenkins_user = os.environ["JENKINS_USER"]
-        jenkins_pass = os.environ["JENKINS_PASS"]
-    except KeyError as e:
-        logging.error("environment variable {} not found".format(e))
-        raise SystemExit
-
-    jenkins_info = {
-        "user": jenkins_user,
-        "pass": jenkins_pass,
-        "url": jenkins_url,
-        "node": node_name
-    }
-
-    docker_host_url = "unix://docker.sock"
-    docker_root_dir = "/docker"
-    workspace_root_dir = "/workspace"
-
     while True:
+        STAGE = 0
         logging.info("***** checking disk usage *****")
 
         try:
-            docker_usage_critical = check_disk_usage(docker_root_dir, threshold)
-            workspace_usage_critical = check_disk_usage(workspace_root_dir, threshold)
+            docker_usage_critical = check_disk_usage(DOCKER_ROOT_DIR)
+            workspace_usage_critical = check_disk_usage(WORKSPACE_ROOT_DIR)
             
             node_offline = False
             if docker_usage_critical or workspace_usage_critical:
                 logging.info("starting cleanup operations")
-                node_offline = make_node_offline(jenkins_info)
+                node_offline = make_node_offline()
 
                 if node_offline:
+                    STAGE = 1
                     if docker_usage_critical:
-                        cleanup_docker(docker_host_url, docker_keep_images_until)
+                        cleanup_docker()
                     if workspace_usage_critical:
-                        cleanup_workspace(workspace_root_dir)
+                        cleanup_workspace()
 
                     logging.info("disk usage after cleanup:")
-                    check_disk_usage(docker_root_dir, threshold)
-                    check_disk_usage(workspace_root_dir, threshold)
+                    check_disk_usage(DOCKER_ROOT_DIR)
+                    check_disk_usage(WORKSPACE_ROOT_DIR)
                     logging.info("finished all cleanup operations")
                 else:
                     logging.warning("skipping cleanup")
@@ -146,10 +138,12 @@ def main():
         except Exception as e:
             logging.error(e)
         finally:
-            if node_offline: make_node_online(jenkins_info)
+            if node_offline:
+                make_node_online()
+                STAGE = 0
 
-        logging.info("***** checking again after {} minute(s) *****".format(minutes))
-        time.sleep(minutes * 60)
+        logging.info("***** checking again after {} minute(s) *****".format(CHECK_INTERVAL))
+        time.sleep(CHECK_INTERVAL * 60)
 
 
 if __name__ == "__main__":
